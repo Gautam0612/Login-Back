98cbaf340dd7b249f030ebb227fc17e9121c0dd0
Gautam Singh Mahara <107118978+jennie0612@users.noreply.github.com>
2023-07-31 19:12:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 try {
     
 mong.connect("mongodb+srv://Jenn:Janki6121@cluster0.vqk5j27.mongodb.net/").then(()=>
- app.listen(5050, ()=>{
+ app.listen(0.0.0.0, ()=>{
     console.log("Server started");
  })
  
